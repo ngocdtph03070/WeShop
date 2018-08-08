1a9882f049cb2d1febb4f61424618c944b24a916
Ngoc Do <ngocdtph03070@gmail.com>
2018-08-08 17:20:34 +0700

    Commit source folder config

---
+++ WeShop/Config.swift
@@ -0,0 +1,30 @@
+//
+//  Config.swift
+//  WeShop
+//
+//  Created by Ngoc Do on 08/08/2018.
+//  Copyright © 2018 Ngoc Do. All rights reserved.
+//
+
+import Foundation
+
+// Cấu hình các dữ liệu
+
+struct Config {
+
+    struct Google {
+        static var APIKey = "Key"
+    }
+    
+    struct Service {
+        static var baseURL = ""
+    }
+    
+    struct Color {
+        static var primaryColor = ""
+    }
+    
+    struct Font {
+        static var fontName = ""
+    }
+}
